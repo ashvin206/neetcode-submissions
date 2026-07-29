f6750df56713ead9c2e8885408cabfbc7c3f16f4
ashvin206 <ashwinvinod206@gmail.com>
2026-07-29 12:38:57 -0700

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,24 @@
+"""
+Go through each word, create letter dictionary
+act = a:1, c:1, t:1 
+Do this for each word
+Instead of dictionary it should be a list of 26 0s, update based on lex
+use that as key for a dictionary, value being list of the words
+Go through that dictionary as they are groupedn ow 
+"""
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = dict() 
+        res = []
+        for word in strs:
+            letters = [0] * 26 
+            for letter in word:
+                letters[ord(letter) - 97] += 1 
+            letters = tuple(letters) 
+            if letters in groups:
+                groups[letters].append(word)
+            else:
+                groups[letters] = [word]
+        for val in groups.values():
+            res.append(val)
+        return res
